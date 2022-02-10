459b141f7f374a58697a6170d4b3a5465c3c3134
sharonGoldshmid <80431983+sharonGoldshmid@users.noreply.github.com>
2022-02-10 22:10:52 +0200

    Update project.py
    Adding documentation, and finall refactoring

--- project.py
+++ project.py
@@ -7,14 +7,13 @@ import time
 # --------------
 # calling and massege:
 
-# You must register on the website and complete the details:
-# https://www.twilio.com/try-twilio
+# You must register on the website and complete the details: https://www.twilio.com/try-twilio
 from_whatsapp = 'whatsapp:_______'    # Twilio Sandbox for WhatsApp (whatsapp:+123456789)
 to_whatsapp = 'whatsapp:_______'     # your number you signed with
+myClient = Client('___ACCOUNT SID___', '___AUTH TOKEN___')
 
 # send a message to my phone, of text or text and a picture with a link
 def send_message(msg, img = 0):
-    myClient = Client('___ACCOUNT SID___', '___AUTH TOKEN___')
     if(img == 0):
         myClient.messages.create(from_=from_whatsapp, to=to_whatsapp, body=msg)
         return
@@ -22,84 +21,102 @@ def send_message(msg, img = 0):
 
 
 # -------------
-# setting:
+# setting of camera:
 
-camera = cv2.VideoCapture(0)  # '0' means leptop camera
-# If Camera Device is not opened, exit the program
-if not camera.isOpened():
+camera = cv2.VideoCapture(0)  # '0' is leptop camera, '1' is a webcam
+
+if not camera.isOpened(): # If Camera Device is not opened, exit the program
     print("Video device or file couldn't be opened")
     exit()
 camera.set(3, 640) # set width
 camera.set(4, 480) # set hight
 
 cv2.namedWindow("video")
-# counter for the names of the pictures
+
+# counter for the names of the pictures, in case we want to save the them on a computer folder
 img_counter = 0
 
 
 #--------------
 # age part:
+# (Based on https://www.thepythoncode.com/article/predict-age-using-opencv)
 
-ageProto="age_deploy.prototxt"
-ageModel="age_net.caffemodel"
+# Initializing the paths of the models' weights and architecture, and loading the models:
 
-MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+ageProto="age_deploy.prototxt"  # The model architecture - containing all the neural network layer’s definitions
+# download from: https://github.com/ntgalili/AI-project-2022
+ageModel="age_net.caffemodel"  # The pre-trained model weights for age detection
+# download from: https://drive.google.com/open?id=1kWv0AjxGSN0g31OeJa02eBGM0R_jcjIl
+
+ageNet=cv2.dnn.readNet(ageModel,ageProto) # Load age prediction model
+
+# The 8 age classes of this CNN probability layer
 ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 
-ageNet=cv2.dnn.readNet(ageModel,ageProto)
-
+MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+# Gets an image of a face as input, and returns an age:
 def age_detected(face):
+    # Input image to preprocess before passing it through our dnn for classification.
     blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+    
+    # Predict Age:
     ageNet.setInput(blob)
     agePreds = ageNet.forward()
     return ageList[agePreds[0].argmax()]
 
 #-------------
 # face part:
+# (Based on https://towardsdatascience.com/face-detection-models-which-to-use-and-why-d263e82c302c)
 
-detector = MTCNN()
+detector = MTCNN() # Load the prediction model
 
+# Gets an image as input, and returns the image with marked squares, and a list of identified ages:
 def face_and_age_detected(image):
-    #image = img.copy()
-    faces = detector.detect_faces(image)  # result
+    faces = detector.detect_faces(image) # faces detection:
+    
     list_of_ages = []
+    
+    # For each of the faces identified we'll predict it age, and print a square on the image
     for result in faces:
         x, y, w, h = result['box']
         x1, y1 = x + w, y + h
 
-        # age detection:
+        # Crop the image for a particular face and send it to age detector
         padding = 20
         face = image[max(0, y - padding):
                      min(y1 + padding, image.shape[0] - 1), max(0, x - padding)
                                                             :min(x1 + padding, image.shape[1] - 1)]
         age = age_detected(face)
         list_of_ages += [age]
+        
+        # draw the rectangle around the face, and write the age
         cv2.rectangle(image, (x, y), (x1, y1), (0, 0, 255), 2)
         cv2.putText(image, age, (x + 5, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
+    
     return image, list_of_ages
 
 
 # ----------------
-cases_count = 0
-check_count = 3
-ok_val = check_count+1
+# settings of the main loop:
+
+# In order to determine whether a warning should be sent or not, we will use the following constants:
+cases_count = 0     # Counts the state values of the images in each round
+check_count = 3     # The number of photos in each round
+ok_val = check_count+1 # Normal condition value - no people / adults only / adult presence with the children
 
 def regVal():
     x = check_count//2
-    return (check_count-x) + ok_val * x
+    return (check_count - x) + (ok_val * x)
+reg_val = regVal()  # A cut line = A line that determines the classification (normal condition or forgetting a child)
 
-reg_val = regVal()
-
-
-#list_age = [[],[],[]]
-
-#Timer for capture every sec seconds
+# Timer for capture a new photo every 'sec' seconds
 timer = 0
-#sec to capture
 sec=2
 
 
+# (The loop based on https://realpython.com/face-detection-in-python-using-a-webcam/)
 while True:
+    # # Capture frame-by-frame
     ret, frame = camera.read()
     if not ret:
         print("failed to grab frame")
@@ -108,31 +125,30 @@ while True:
 
     k = cv2.waitKey(1)
     if k%256 == 27:
-        # ESC pressed
+        # ESC pressed - closing the program
         print("Escape hit, closing...")
         break
 
+    # If waiting time is over 'sec' seconds:
+    elif (time.time() - timer >= sec):
+        timer = time.time() # update timer
 
-        # If waiting time is over sec
-    elif (time.time() - timer >= sec):         # k%256 == 32:# SPACE pressed #
-        timer = time.time() #update timer
-
-
-        # # save the pictures in Folder pictures
+        """ save the pictures in Folder pictures:
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        directory = r"C:\Users\office\PycharmProjects\openCVpython\images" # the relevant path
+        #Set a location for saving the captured images
+        os.chdir(directory)
+        cv2.imwrite(img_name, frame)
+        #print("{} written!".format(img_name))
+        img = cv2.read(directory + "\\" + img_name) """
+        
+        result_image, list_of_age = face_and_age_detected(frame)   # get image with mark faces and the list of the ages
         img_name = "opencv_frame_{}.png".format(img_counter%check_count)
-        # directory = r"C:\Users\office\PycharmProjects\openCVpython\images"
-        # #Set a location for saving the captured images
-        # os.chdir(directory)
-        # cv2.imwrite(img_name, frame)
-        # #print("{} written!".format(img_name))
 
-
-        #img = cv2.read(directory + "\\" + img_name)
-        result_image, list_of_age = face_and_age_detected(frame)
-        # get image with mark faces and the list of the ages
-
-        # cv2.imshow(img_name, result_image)
         cv2.imshow(img_name, result_image)
+        img_counter += 1
+        
+        # Checking the condition of the current frame according to the identified ages:
         if ('(0-2)' in list_of_age or
                 '(4-6)' in list_of_age or
                 '(8-12)' in list_of_age):
@@ -142,19 +158,17 @@ while True:
                     '(38-43)' in list_of_age or
                     '(48-53)' in list_of_age or
                     '(60-100)' in list_of_age):
+                # print("there is an adult with the child")
                 cases_count += ok_val
-                # print("there is an adult")
             else:
-                # create_call()
+                # print("children only")
                 cases_count += 1
-
         else:
-            # print("there is not children under 6")
+            # print("no children")
             cases_count += ok_val
 
-        img_counter += 1
 
-        # Check each check_count photos what the condition of the vehicle is
+        # Checking each 'check_count' photos what the condition of the vehicle is:
         if img_counter % check_count == 0:
             if cases_count <= reg_val:
                 #print("call")
@@ -162,11 +176,9 @@ while True:
             else:
                 #print("all ok")
                 send_message("all ok")
-
             cases_count = 0
-
+            
 
 camera.release()
 cv2.destroyAllWindows()
-
 cv2.waitKey(0)
